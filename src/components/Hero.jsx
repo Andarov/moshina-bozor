6ce22ebfb192da199790dcbcf4d898f706266665
Andarov <bekturandarob678@gmail.com>
2023-12-27 21:12:01 +0500

    Add bg no repeat and bg cover to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='bg-heroBg min-h-96 w-full pt-12 bg-111/40'>
+    <div className='bg-heroBg bg-no-repeat bg-cover min-h-96 w-full pt-12 bg-111/40'>
         <div className="w-full max-w-base mx-auto px-5">
             <p className='text-4xl leading-[52px] font-bold text-white'>Финальная ликвидация склада Авто!
             <br />
