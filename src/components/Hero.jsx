ca8c7760fb19614bba293a5151c6bdb08beb8eae
Andarov <bekturandarob678@gmail.com>
2024-01-09 12:20:02 +0500

    Translate hero section texts rus to uzb

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <div className='bg-heroBg bg-no-repeat bg-cover min-h-96 w-full pt-12 bg-111/40'>
         <div className="w-full max-w-base mx-auto px-5">
-            <p className='text-4xl leading-[52px] font-bold text-white'>Финальная ликвидация склада Авто!
+            <p className='text-4xl leading-[52px] font-bold text-white'>Moshina bozor bilan hammasi oson
             <br />
-            До 15-го апреля!</p>
+            Oson soting va sotib oling !</p>
         </div>
     </div>
   )
