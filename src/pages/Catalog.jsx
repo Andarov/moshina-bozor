80cd3d77716173458461602b527e75a7baf9f986
Andarov <bekturandarob678@gmail.com>
2024-01-12 23:08:18 +0500

    Create pagination component and add to Catalog page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,8 +1,11 @@
+// Catalog.jsx
+
 import React, { useRef, useState } from "react";
 import { cars } from "../data";
 import CarItem from "../components/CarItem";
 import { Link, useParams } from "react-router-dom";
 import Filter from "../components/Filter";
+import Pagination from "../components/Pagination";
 
 const Catalog = () => {
   const { model, marka } = useParams();
@@ -10,9 +13,6 @@ const Catalog = () => {
   const carsOfMarka = cars.filter((car) => car.marka === marka);
   const targetArray = carsOfModel.length > 0 ? carsOfModel : cars;
   const markaArray = carsOfMarka.length > 0 ? carsOfMarka : targetArray;
-
-  // const isModelPage = !!model;
-  // console.log(isModelPage);
 
   const catalogRef = useRef(null);
   const [currentPage, setCurrentPage] = useState(1);
@@ -65,7 +65,7 @@ const Catalog = () => {
         )}
 
         {/* filtr */}
-        <Filter markaArray={markaArray}/>
+        <Filter markaArray={markaArray} />
 
         <ul className="grid grid-cols-3 gap-x-7 gap-y-10">
           {markaArray.slice(startIndex, endIndex).map((car) => (
@@ -74,45 +74,11 @@ const Catalog = () => {
         </ul>
 
         {/* Pagination buttons */}
-        <div className="flex justify-center mt-10">
-          <button
-            onClick={() => handlePageChange(currentPage - 1)}
-            className={`${
-              currentPage === 1
-                ? "bg-gray-200 text-gray-700"
-                : "bg-teal-500 text-white"
-            }  font-semibold rounded-md px-4 py-2 mx-1`}
-            disabled={currentPage === 1}
-          >
-            Previous
-          </button>
-
-          {Array.from({ length: totalPages }, (_, index) => (
-            <button
-              key={index}
-              onClick={() => handlePageChange(index + 1)}
-              className={`${
-                currentPage === index + 1
-                  ? "bg-teal-500 text-white"
-                  : "bg-gray-200 text-gray-700"
-              } font-semibold rounded-md px-4 py-2 mx-1`}
-            >
-              {index + 1}
-            </button>
-          ))}
-
-          <button
-            onClick={() => handlePageChange(currentPage + 1)}
-            className={`${
-              currentPage === totalPages
-                ? "bg-gray-200 text-gray-700"
-                : "bg-teal-500 text-white"
-            } font-semibold rounded-md px-4 py-2 mx-1`}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
-        </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </div>
   );
