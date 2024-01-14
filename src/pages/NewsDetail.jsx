0dedc641c690eb31fe24924251a2a4bc4bddc076
Andarov <bekturandarob678@gmail.com>
2024-01-14 21:55:43 +0500

    Add responsive styles to news detail page

--- src/pages/NewsDetail.jsx
+++ src/pages/NewsDetail.jsx
@@ -7,12 +7,12 @@ const NewsDetail = () => {
   const newsItem = news.find((newsItem)=> newsItem.title === title);
 
   return (
-    <div className="pb-20">
+    <div className="pb-10 md:pb-20">
       <div className="w-full max-w-base mx-auto px-5">
-        <h1 className="text-4xl leading-10 text-111 font-bold mt-5 mb-8">
+        <h1 className="text-[25px] leading-7 text-111 font-bold mb-7 lg:text-4xl md:leading-8 md:mb-8">
           {newsItem.title}
         </h1>
-        <img className="w-full rounded-3xl mb-6 h-96 object-cover" src={newsItem.img} alt={newsItem.title} />
+        <img className="w-full rounded-xl mb-6 h-96 object-cover md:rounded-2xl lg:rounded-3xl" src={newsItem.img} alt={newsItem.title} />
         <p className="text-base mb-10 text-111 font-medium">{newsItem.text}</p>
 
         <div className="text-right">
