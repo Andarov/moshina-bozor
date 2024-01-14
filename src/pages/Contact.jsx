e10821197ccf905aed1321379d676d959f7759a9
Andarov <bekturandarob678@gmail.com>
2024-01-14 21:20:38 +0500

    Add responsive styles to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,20 +5,20 @@ const Contact = () => {
     return (
     <div className="pb-12">
       <div className="w-full max-w-base mx-auto px-5">
-        <h1 className="text-4xl leading-10 text-111 font-bold mt-5 mb-8">
+        <h1 className="text-3xl leading-7 text-111 font-bold mb-7 md:text-4xl md:leading-8 md:mb-8">
           Boglanish
         </h1>
 
         {/* short infos */}
-        <ul className="grid grid-cols-3 gap-7 mb-10">
+        <ul className="grid grid-cols-1 gap-7 mb-10 md:grid-cols-2 lg:grid-cols-3">
           {
             contactInfos.map((contactInfo)=>{
                 return(
                     <li key={contactInfo.id} className="p-5 rounded-3xl flex items-center shadow-lg border">
                         <img className="mr-5" src={contactInfo.img} alt={contactInfo.title} />
                         <div>
-                            <h3 className="text-lg font-bold mb-1">{contactInfo.title}</h3>
-                            <p>{contactInfo.text}</p>
+                            <h3 className="text-base font-bold mb-1 sm:text-lg">{contactInfo.title}</h3>
+                            <p className="text-sm sm:text-base">{contactInfo.text}</p>
                         </div>
                     </li>
                 )
@@ -27,7 +27,7 @@ const Contact = () => {
         </ul>
 
         {/* all infos */}
-        <div className="grid grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
           <div>
             <h2 className="text-lg font-bold leading-6 text-111 mb-5">Umumiy ma'lumotlar:</h2>
 
@@ -50,7 +50,7 @@ const Contact = () => {
                 </div>
             </div>
           </div>
-          <div className="w-full max-w-[600px] bg-gray-400">
+          <div className="w-full bg-gray-400 lg:max-w-[600px]">
           <iframe className="w-full h-[400px] border-0" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6497.36229050489!2d72.34037696855025!3d40.808700407126736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1704911053862!5m2!1sen!2s" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
