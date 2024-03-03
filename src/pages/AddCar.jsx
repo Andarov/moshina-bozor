2e717891f0678140fd1ec7b12e44899f92c3cecf
Andarov <bekturandarob678@gmail.com>
2024-03-03 23:56:48 +0500

    Add responsive styles to add car page

--- src/pages/AddCar.jsx
+++ src/pages/AddCar.jsx
@@ -6,7 +6,7 @@ const AddCar = () => {
       <h1 className="text-3xl leading-7 text-111 font-bold mb-7 md:text-4xl md:leading-8 md:mb-8">E'lon joylash</h1>
       <hr className='mb-14' />
 
-      <form className='grid grid-cols-3 gap-6'>
+      <form className='grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3'>
         {/* ism */}
         <input className='px-6 py-3 border-2 rounded-lg' type="text" name="Ismingiz" placeholder='Ismingiz' />
         {/* tel */}
@@ -26,10 +26,10 @@ const AddCar = () => {
         {/* rang */}
         <input className='px-6 py-3 border-2 rounded-lg' type="text" name="Rangi" placeholder='Rangi' />
         {/* rasm */}
-        <label className="px-6 py-3 border-2 rounded-lg col-span-3 text-center" htmlFor="image-input">Mashina rasmlarini joylang</label>
+        <label className="px-6 py-3 border-2 rounded-lg col-span-1 text-center md:col-span-2 lg:col-span-3" htmlFor="image-input">Mashina rasmlarini joylang</label>
         <input id="image-input" className="hidden" type="file" name="mashina rasmlari" accept=".jpg, .jpeg, .png" multiple=""></input>
         {/* extra */}
-        <textarea className='px-6 py-3 border-2 rounded-lg resize-none col-span-3' name="Qoshimcha malumotlar" placeholder="Qo'shimcha malumotlar"></textarea>
+        <textarea className='min-h-40 px-6 py-3 border-2 rounded-lg resize-none col-span-1 md:col-span-2 lg:col-span-3' name="Qoshimcha malumotlar" placeholder="Qo'shimcha malumotlar"></textarea>
       </form>
     </div>
   )
