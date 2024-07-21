42ade2fcb4b5d95f398c569ccc634c9d5b4096b8
Andarov <bekturandarob678@gmail.com>
2024-07-21 15:16:49 +0500

    Add some comments to car comment item component

--- src/components/CarCommentItem.jsx
+++ src/components/CarCommentItem.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const CarCommentItem = ({ id, model, marka, author, rate, liked, comment }) => {
-  // Yulduzcha rangini belgilash uchun funksiyamiz
+  // Yulduzcha rangini belgilash uchun
   const getStarColor = (rate) => {
-    if (rate <= 2) return "text-red-500"; // qizil
-    if (rate === 3) return "text-yellow-500"; // sariq
-    if (rate === 4) return "text-green-300"; // och yashil
-    if (rate === 5) return "text-green-700"; // to'q yashil
-    return "text-gray-500"; // default rang
+    if (rate <= 2) return "text-red-500";
+    if (rate === 3) return "text-yellow-500";
+    if (rate === 4) return "text-green-300";
+    if (rate === 5) return "text-green-700";
+    return "text-gray-500";
   };
 
-  // Yulduzchalarni ko'rsatish uchun funksiyamiz
+  // Vaziyatga qarab yulduzchalar holati
   const renderStars = (rate) => {
-    const totalStars = 5; // Umumiy yulduzcha soni
+    const totalStars = 5; 
     const stars = [];
     const starColor = getStarColor(rate);
     for (let i = 0; i < totalStars; i++) {
@@ -33,11 +33,11 @@ const CarCommentItem = ({ id, model, marka, author, rate, liked, comment }) => {
           <svg
             key={i}
             xmlns="http://www.w3.org/2000/svg"
-            className={`h-5 w-5 text-gray-400`} // Gray color for border only stars
+            className={`h-5 w-5 text-gray-400`}
             viewBox="0 0 20 20"
             fill="none"
             stroke="currentColor"
-            strokeWidth="1" // Thinner border
+            strokeWidth="1"
           >
             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.43 4.399a1 1 0 00.95.69h4.657c.969 0 1.371 1.24.588 1.81l-3.757 2.73a1 1 0 00-.364 1.118l1.43 4.398c.3.921-.755 1.688-1.54 1.118l-3.757-2.73a1 1 0 00-1.175 0l-3.757 2.73c-.784.57-1.84-.197-1.54-1.118l1.43-4.398a1 1 0 00-.364-1.118L2.173 9.826c-.784-.57-.38-1.81.588-1.81h4.657a1 1 0 00.95-.69l1.43-4.399z" />
           </svg>
