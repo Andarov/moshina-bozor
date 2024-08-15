e3f134a856ff1e571b7a1821b9a2531887519c39
yaxyobekb <pubgn9642@gmail.com>
2024-08-15 11:45:38 +0500

    update sign in page styles

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,27 +1,110 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+// images
+import malibu from "../img/favicon-40x40.svg";
 
 const SignIn = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
-        <form>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email</label>
-            <input type="email" className="w-full p-2 border rounded" />
+    <>
+      {/* logo */}
+      <Link
+        to="/"
+        aria-label="home"
+        title="mashina bozor"
+        className="flex items-center gap-2 text-xl text-white font-semibold leading-none"
+      >
+        {/* logo img */}
+        <img
+          width={44}
+          height={44}
+          src={malibu}
+          alt="chevrolet malibu"
+          className="w-9 h-9 sm:h-10 md:w-11 md:h-11"
+        />
+
+        {/* logo text */}
+        <span>
+          <span className="text-main">Mashina </span>
+          <span className="text-111">bozor</span>
+        </span>
+      </Link>
+
+      {/* mid content */}
+      <div className="space-y-10">
+        {/* title wrapper */}
+        <div className="space-y-5">
+          {/* title */}
+          <h1 className="text-2xl font-bold sm:text-[28px] sm:leading-normal">
+            Akkauntga kirish
+          </h1>
+
+          {/* description */}
+          <p className="opacity-70 sm:font-medium">
+            Akkauntga kirish uchun ma'lumotlaringizni kiriting
+          </p>
+        </div>
+
+        {/* login form */}
+        <form className="space-y-6">
+          {/* email */}
+          <div className="space-y-3">
+            <label htmlFor="email" className="font-medium">
+              E-pochta*
+            </label>
+
+            {/* input */}
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="misol@gmail.com"
+            />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700">Password</label>
-            <input type="password" className="w-full p-2 border rounded" />
+
+          {/* password */}
+          <div className="space-y-3">
+            <label htmlFor="password" className="font-medium">
+              Parol*
+            </label>
+
+            {/* input */}
+            <input
+              type="text"
+              id="password"
+              name="password"
+              placeholder="parol1234"
+            />
           </div>
-          <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Sign In</button>
+
+          {/* forgot password */}
+          <p className="font-medium">
+            <span>Parolni unutdingizmi? </span>
+
+            {/* link */}
+            <Link className="text-teal-500">Qayta tiklash</Link>
+          </p>
+
+          {/* submit btn */}
+          <button type="submit">Kirish</button>
         </form>
-        <div className="text-center mt-4">
-          <Link to="/signup" className="text-blue-500">Ro'yhatdan o'tish</Link>
+
+        {/* sign up link */}
+        <div className="font-medium">
+          <span>Akkauntingiz yo'qmi? </span>
+
+          {/* link */}
+          <Link to="/auth/signup" className="text-teal-500">
+            Ro'yxatdan o'tish
+          </Link>
         </div>
       </div>
-    </div>
+
+      {/* sub content */}
+      <p className="opacity-70">
+        © 2024 - Mashina bozor. Barcha huquqlar himoyalangan
+      </p>
+    </>
   );
 };
 
