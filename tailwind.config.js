fc8df78868de42b86f2298d5257e677a190338d2
yaxyobekb <pubgn9642@gmail.com>
2024-08-17 16:56:50 +0500

    format tailwind.config.js codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,43 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'main': '#E70A32',
-        '111': '#111'
+      colors: {
+        main: "#E70A32",
+        111: "#111",
       },
-      fontFamily:{
-        'montserrat': ['Montserrat', 'sans-serif']
+
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
       },
-      maxWidth:{
-        'base': '1210px'
+
+      maxWidth: {
+        base: "1210px",
       },
-      backgroundImage:{
-        'heroBg': "linear-gradient(180deg, rgba(9, 9, 9, 0.70) 0%, #09161F 1000%), url('./img/heroBg.jpg')",
-        'detailBg': "linear-gradient(180deg, rgba(9, 9, 9, 0.70) 0%, #09161F 1000%), url('./img/banerDetail.webp')",
+
+      maxHeight: {
+        lg: "984px",
       },
+
+      minHeight: {
+        sm: "540px",
+      },
+
+      backgroundImage: {
+        hero: "linear-gradient(180deg, rgba(9, 9, 9, 0.50) 0%, #09161F 1000%), url('./img/bg/hero.jpg')",
+        detailBg:
+          "linear-gradient(180deg, rgba(9, 9, 9, 0.70) 0%, #09161F 1000%), url('./img/banerDetail.webp')",
+      },
+
       boxShadow: {
-        'header-shadow': '0px 5px 20px 0px rgba(44, 41, 51, 0.10)',
+        "header-shadow": "0px 5px 20px 0px rgba(44, 41, 51, 0.10)",
       },
+
       dropShadow: {
         "header-logo-shadow": "0px 0px 2px rgba(0,0,0,0.4)",
       },
     },
   },
   plugins: [],
-})
+});
