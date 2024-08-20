fd1f917cebc7e2ed77c59265ec3e3f3a38dcaa9e
yaxyobekb <pubgn9642@gmail.com>
2024-08-20 10:05:45 +0500

    update homepage news section styles

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+
+// data
 import { news } from "../data";
+
+// components
 import NewsItem from "./NewsItem";
 
 const News = () => {
   return (
     <section className="pt-7">
-      <div className="w-full max-w-base mx-auto px-5">
+      <div className="container">
         <h2 className="text-2xl leading-7 text-111 font-bold mb-8 sm:text-3xl md:leading-9">
           So'nggi yangiliklar
         </h2>
 
-        <ul className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3">
+        {/* news list */}
+        <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:gap-6 lg:grid-cols-3">
           {news.slice(0, 3).map((newsItem) => {
-            return (
-              <NewsItem key={newsItem.id} {...newsItem} />
-            );
+            return <NewsItem key={newsItem.id} {...newsItem} />;
           })}
         </ul>
       </div>
