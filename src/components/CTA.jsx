e2f924f31ce36edcc56e2b897dd3aa55c4d9dc64
yaxyobekb <pubgn9642@gmail.com>
2024-08-24 20:33:27 +0500

    update CTA section styles

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,30 +1,61 @@
 import React from "react";
-import carIcon from '../img/carIcon.png'
-import telegramIcon from '../img/telegramIcon.png'
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+
+// images
+import moreIcon from "../img/icon/more-circle.svg";
+import telegramIcon from "../img/icon/telegram-filled.svg";
 
 const CTA = () => {
   return (
-    <div className="py-20 bg-detailBg bg-cover">
-      <div className="w-full max-w-base mx-auto px-5">
-        <div className="w-full max-w-md text-white space-y-4">
+    <section className="py-20 bg-detailBg bg-cover">
+      <div className="container">
+        <div className="w-full max-w-md space-y-6 text-white">
+          {/* title */}
           <h2 className="text-2xl leading-7 font-bold sm:text-3xl md:leading-9">
             Biz bilan bog'lanish
           </h2>
-          <p className="opacity-80 font-medium">Saytga o'z mashinangiz elonini joylamoqchimisiz yoki sayt bo'yicha takliflaringiz bor bo'lsa quyidagi linklar orqali bog'lanishingiz mumkin</p>
-          <div className="flex items-center space-x-4">
-            <a className="inline-flex items-center px-3 py-3 bg-main rounded-lg space-x-2 sm:px-8 sm:space-x-4" href="https://t.me/Andarov_B">
-                <img className="w-6 h-6" width={24} height={24} src={telegramIcon} alt="Telegram Icon" />
-                <span className="font-semibold">Telegram</span>
+
+          {/* description */}
+          <p className="opacity-80 font-medium">
+            Sizda muammo yoki taklif bormi? Unday bo'lsa biz bilan tezkor
+            bog'laning! Barcha murojaatlar 3 ish kuni ichida ko'rib chiqiladi.
+          </p>
+
+          {/* icons wrapper */}
+          <div className="flex flex-col gap-6 sm:flex-row">
+            <a
+              target="_blank"
+              href="https://t.me/Andarov_B"
+              className="flex items-center justify-center w-full gap-5 py-3 bg-main rounded-lg transition-colors duration-200 hover:bg-main/80 sm:w-52"
+            >
+              <img
+                width={24}
+                height={24}
+                src={telegramIcon}
+                className="size-6"
+                alt="telegram Icon"
+              />
+              <span className="font-medium">Telegram</span>
             </a>
-            <Link to='/elon' className="inline-flex items-center px-3 py-3 bg-main rounded-lg space-x-2 sm:px-8 sm:space-x-4">
-                <img className="w-6 h-6" width={24} height={24} src={carIcon} alt="Ad Icon" />
-                <span className="font-semibold">Elon joylash</span>
+
+            {/* more */}
+            <Link
+              to="/contact"
+              className="flex items-center justify-center w-full gap-5 py-3 bg-main rounded-lg transition-colors duration-200 hover:bg-main/80 sm:w-52"
+            >
+              <img
+                width={24}
+                height={24}
+                src={moreIcon}
+                alt="more Icon"
+                className="size-6"
+              />
+              <span className="font-medium min-w-[76px]">Ko'proq</span>
             </Link>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
