1aa295cb5e339437f8593d890a706b1122131d3d
yaxyobekb <pubgn9642@gmail.com>
2024-08-24 20:46:10 +0500

    update homepage comments section styles

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { carCommentsWithImage } from '../data'
-import CarCommentItem from './CarCommentItem'
+import React from "react";
+import { Link } from "react-router-dom";
+
+// components
+import CarCommentItem from "./CarCommentItem";
+
+// data
+import { carCommentsWithImage } from "../data";
 
 const Comments = () => {
   return (
-    <section className='bg-gray-200 py-14'>
-      <div className='w-full max-w-base mx-auto px-5'>
-        <div className='flex justify-between items-center mb-10'>
-          <h2 className='text-2xl leading-7 text-111 font-bold sm:text-3xl md:leading-9'>Moshinalar haqida sharxlar</h2>
+    <section className="bg-[#EFF2F4] py-14">
+      <div className="container">
+        {/* content top */}
+        <div className="flex justify-between items-center mb-10">
+          <h2 className="text-2xl leading-7 text-111 font-bold sm:text-3xl">
+            Mashinalar haqida sharxlar
+          </h2>
 
-          <Link to='/rating' className='underline underline-offset-4'>Barcha sharxlar</Link>
+          <Link
+            to="/rating"
+            className="font-medium underline underline-offset-4 transition-colors hover:text-main"
+          >
+            Barcha sharxlar
+          </Link>
         </div>
 
-        <ul className="grid grid-cols-1 gap-7 md:gap-y-10 md:grid-cols-2 lg:grid-cols-3">
-          {carCommentsWithImage.slice(0,6).map((car) => (
+        {/* content main */}
+        <ul className="grid grid-cols-1 gap-x-5 gap-y-6 md:gap-y-5 md:grid-cols-2 lg:grid-cols-3">
+          {carCommentsWithImage.slice(0, 6).map((car) => (
             <CarCommentItem key={car.id} {...car} />
           ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
