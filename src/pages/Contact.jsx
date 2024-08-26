98063f03027cc8f6e8c8fb298b0a0ec63302e75a
yaxyobekb <pubgn9642@gmail.com>
2024-08-26 06:45:50 +0500

    fix invalid dom property on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -134,11 +134,10 @@ const Contact = () => {
           {/* map */}
           <div className="w-full bg-gray-400">
             <iframe
-              className="w-full h-[400px] border-0"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6497.36229050489!2d72.34037696855025!3d40.808700407126736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1704911053862!5m2!1sen!2s"
-              allowfullscreen=""
               loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
+              className="w-full h-[400px] border-0"
+              referrerPolicy="no-referrer-when-downgrade"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6497.36229050489!2d72.34037696855025!3d40.808700407126736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1704911053862!5m2!1sen!2s"
             ></iframe>
           </div>
         </section>
